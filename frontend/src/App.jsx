6256b89c32e49529d2bdd1e0e2213abfe6302b09
ben <gomben397@hengersor.hu>
2025-03-03 15:02:08 +0100

    navbar átrakva külön fileba

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -25,53 +25,14 @@ function App() {
 
   return (
     <>
-      <div className='App'>
-        <nav className='navbar container sticky-nav' role='navigation' aria-label='main navigation'>
-          <div className='navbar-brand'>
-            <Link to={"/"} id="homeScratch" className='navbar-item is-size-4'>
-            <img className='img-responsive' style={{width: "60px"}} src={snpLogo} alt="" />
-                Scratch 'N Spin
-            </Link>
-            <label role='button' class='navbar-burger burger' aria-label='menu' aria-expanded="false" data-target='navbarBasicExample' 
-            onClick={e =>{
-              e.preventDefault();
-              this.setState({showMenu: !this.state.showMenu})
-            }}>
-              <span aria-hidden="true"></span>
-              <span aria-hidden="true"></span>
-              <span aria-hidden="true"></span>
-            </label>
-          </div>
-          <div className={`navbar-menu`}>
-            <Link to={"/"} className='navbar-item'>
-              Termékeink
-                <span
-                    className="tag is-primary"
-                    style={{ marginLeft: "5px" }}
-                  >
-                    </span>
-            </Link>
-            <Link to="/cart" className="navbar-item">
-                  Cart
-                  <span
-                    className="tag is-primary"
-                    style={{ marginLeft: "5px" }}
-                  >
-                  </span>
-            </Link>
-                  <Link to="/login" className="navbar-item">
-                    Login
-                  </Link>
-          </div>
-        </nav>
-      </div>
-        <Routes>
-          <Route index element={<Bakelitek />}/>
-          <Route path="/item/:itemId" element={<ItemView />}/>
-          <Route Component={Error}></Route>
-        </Routes>
+      <BrowserRouter>
+        <Navbar />
+          <Routes>
+            <Route index element={<Bakelitek />}/>
+            <Route path="/item/:itemId" element={<ItemView />}/>
+            <Route Component={Error}></Route>
+          </Routes>
       </BrowserRouter>
-      </div>
     </>
   )
 }
