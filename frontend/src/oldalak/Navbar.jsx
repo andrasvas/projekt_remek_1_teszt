f263c4106e2f3456165be6bb1909cd8ac2f4a8a4
ben <gomben397@hengersor.hu>
2025-03-04 12:26:06 +0100

    nem angol az oldal hanem magyar

--- frontend/src/oldalak/Navbar.jsx
+++ frontend/src/oldalak/Navbar.jsx
@@ -8,44 +8,56 @@ import snpLogo from '../assets/a_logo.png'
 function Navbar(){
 
     return(
-    <nav className='navbar container sticky-nav' role='navigation' aria-label='main navigation'>
-        <div className='navbar-brand'>
-        <Link to={"/"} id="homeScratch" className='navbar-item is-size-4'>
-        <img className='img-responsive' style={{width: "60px"}} src={snpLogo} alt="" />
-            Scratch 'N Spin
-        </Link>
-        <label role='button' class='navbar-burger burger' aria-label='menu' aria-expanded="false" data-target='navbarBasicExample' 
-        onClick={e =>{
-            e.preventDefault();
-            this.setState({showMenu: !this.state.showMenu})
-        }}>
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-        </label>
-        </div>
-        <div className={`navbar-menu`}>
-        <Link to={"/"} className='navbar-item'>
-            Termékeink
-            <span
-                className="tag is-primary"
-                style={{ marginLeft: "5px" }}
-                >
-                </span>
-        </Link>
-        <Link to="/cart" className="navbar-item">
-                Cart
-                <span
-                className="tag is-primary"
-                style={{ marginLeft: "5px" }}
-                >
-                </span>
-        </Link>
-                <Link to="/login" className="navbar-item">
-                Login
-                </Link>
+
+    <nav className="flex justify-between bb b--white-10">
+        <a className="link white-70 hover-white no-underline flex items-center pa3" href="">
+            <img className='img-responsive' style={{width: "60px"}} src={snpLogo} alt="" />
+        </a>
+        <div className="flex-grow pa3 flex items-center">
+            <a className="f6 link dib white dim mr3 mr4-ns" href="#0">About</a>
+            <a className="f6 link dib white dim mr3 mr4-ns" href="#0">Sign In</a>
+            <a className="f6 dib white bg-animate hover-bg-white hover-black no-underline pv2 ph4 br-pill ba b--white-20" href="#0">Sign Up</a>
         </div>
     </nav>
+
+    // <nav className='navbar navbar-form navbar-fixed-top container sticky-nav' role='navigation' aria-label='main navigation'>
+    //     <div className='navbar-brand'>
+    //     <Link to={"/"} id="homeScratch" className='navbar-item is-size-4'>
+    //     <img className='img-responsive' style={{width: "60px"}} src={snpLogo} alt="" />
+    //         Scratch 'N Spin
+    //     </Link>
+    //     <label role='button' class='navbar-burger burger' aria-label='menu' aria-expanded="false" data-target='navbarBasicExample' 
+    //     onClick={e =>{
+    //         e.preventDefault();
+    //         this.setState({showMenu: !this.state.showMenu})
+    //     }}>
+    //         <span aria-hidden="true"></span>
+    //         <span aria-hidden="true"></span>
+    //         <span aria-hidden="true"></span>
+    //     </label>
+    //     </div>
+    //     <div className={`navbar-menu`}>
+    //     <Link to={"/"} className='navbar-item'>
+    //         Termékeink
+    //         <span
+    //             className="tag is-primary"
+    //             style={{ marginLeft: "5px" }}
+    //             >
+    //             </span>
+    //     </Link>
+    //     <Link to="/cart" className="navbar-item">
+    //             Cart
+    //             <span
+    //             className="tag is-primary"
+    //             style={{ marginLeft: "5px" }}
+    //             >
+    //             </span>
+    //     </Link>
+    //             <Link to="/login" className="navbar-item">
+    //             Login
+    //             </Link>
+    //     </div>
+    // </nav>
     )
 }
 
