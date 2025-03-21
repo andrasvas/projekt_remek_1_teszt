b50fbd11e40a1f0843168c33dd51fdae52244b95
andrasvas <asulismodszer@gmail.com>
2025-03-21 11:28:52 +0100

    Update Navbar.jsx

--- frontend/src/oldalak/Navbar.jsx
+++ frontend/src/oldalak/Navbar.jsx
@@ -17,7 +17,7 @@ function Navbar(){
         </a>
         <div className="flex-grow pa3 flex items-center">
             <a className="f6 link dib hover-black mr3 mr4-ns text-nowrap" href="/about">About</a>
-            <a className='f6 link dib hover-black mr3 mr4-ns text-nowrap' href=''>Cart</a>
+            <a className='f6 link dib hover-black mr3 mr4-ns text-nowrap' href='/cart'>Cart</a>
             {isLoggedIn?null:<a className="f6 link dib hover-black mr3 mr4-ns text-nowrap" href="/signin">Sign In</a>}
             {isLoggedIn?null:<a className="f6 dib black bg-animate hover-bg-white hover-black-pill no-underline pv2 ph4 br-pill text-nowrap ba" href="/signup">Sign Up</a>}
             {isLoggedIn?<a className="f6 link dib hover-black mr3 mr4-ns text-nowrap" href="/profile">My Profile</a>:null}
