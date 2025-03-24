998f3639f8f74d7ca2e7d689fdfd81e843158545
ben <gomben397@hengersor.hu>
2025-03-24 10:01:31 +0100

    multkor nem commitoltam

--- frontend/src/oldalak/About.jsx
+++ frontend/src/oldalak/About.jsx
@@ -14,31 +14,16 @@ import 'bootstrap/dist/js/bootstrap.min.js'
 function About(){
 
     return(
-<<<<<<< Updated upstream
         <div className="m-auto">
                 <div className="m-auto">
                     <div >
-                        <div className="m-auto">
-                            <h2 className="m-auto secondary-brand mb-3">Üdvözlünk az oldalon!</h2>
-                            <p className="m-auto main-brand">A bakelitlemezek szerelmeseinek és zenebarátoknak szóló elsőszámú online célponton!</p>
-                            <p className="m-auto main-brand"> Útunk egy egyszerű, 
-                            mégis szenvedélyes célból indult: a ritkább, exotikus bakelit lemezeket szeretnénk eljuttatni mindenki számára, 
-                            megőrizve az analóg hang élményét a digitális korban.</p>  
-                            
-                            <h2 className="m-auto secondary-brand my-3 mt-5">Mi a küldetésünk?</h2>
-                            <p className="m-auto main-brand"> Scratch 'N Spin Records-nál arra törekszünk, hogy egy sokszínű és magas minőségű bakelitlemez-kollekciót állítsunk össze, 
-=======
-        <div className="">
-                <div>
-                    <div>
-                        <div>
+                        <div className="bg-danger">
                             <h2 className="m-auto main-brand mb-3">Üdvözlünk az oldalon!</h2>
                             <p className="m-auto">A bakelitlemezek szerelmeseinek és zenebarátoknak szóló elsőszámú online célponton! Útunk egy egyszerű, 
                             mégis szenvedélyes célból indult: a ritkább, exotikus bakelit lemezeket szeretnénk eljuttatni mindenki számára, 
                             megőrizve az analóg hang élményét a digitális korban. </p>
                             <h2 className="m-auto main-brand mb-3 mt-5">Mi a küldetésünk?</h2>
                             <p className="m-auto"> Scratch 'N Spin Records-nál arra törekszünk, hogy egy sokszínű és magas minőségű bakelitlemez-kollekciót állítsunk össze, 
->>>>>>> Stashed changes
                             amely átível a zenei műfajokon és korokon. 
                             Célunk, hogy felülmúlhatatlan vásárlási élményt nyújtsunk vásárlóinknak, kényelmet és nosztalgiát egyaránt kínálva. </p>
                             <p className="m-auto main-brand">
@@ -47,13 +32,8 @@ function About(){
                             </p> 
                         </div>
                     <div>
-<<<<<<< Updated upstream
-                        <h2 className="m-auto secondary-brand my-3 mt-5">Miért is hoztunk létre a Scratch 'N Spin Recordsot?</h2>
-                        <p className="m-auto main-brand">Egy olyan korban, amikor a digitális streamelés uralkodik, felismertük a vinyllemezek tartós vonzerejét. 
-=======
-                        <h2 className="m-auto main-brand mb-3 mt-5">Miért is hoztuk létre a Scratch 'N Spin Recordsot?</h2>
+                        <h2 className="m-auto main-brand my-3 mt-5">Miért is hoztunk létre a Scratch 'N Spin Recordsot?</h2>
                         <p className="m-auto">Egy olyan korban, amikor a digitális streamelés uralkodik, felismertük a vinyllemezek tartós vonzerejét. 
->>>>>>> Stashed changes
                             A lemez megfogása, a lejátszóra helyezése és a meleg, gazdag hang, amit produkál, mind része egy egyedi és élvezetes hallgatási élménynek. 
                             A Scratch 'N Spin Records abból a vágyból született, hogy ezt a hagyományt életben tartsuk és mindenkinek hozzáférhetővé tegyük.</p>
                         <p className="m-auto main-brand">Minden támogatást és lelkesedőért nagyon hálásak vagyunk és, ha meg is fogtunk a bemutatkozásunkal akkor tarts velünk nyugodtan!</p>
@@ -61,65 +41,23 @@ function About(){
                     </div>
                 </div>
         </div>
-<<<<<<< Updated upstream
             <h3 className="main-brand m-auto">Made by:</h3>
-            <div className=" container text-center">
-                    <div className="row d-flex justify-content-center  ">
+            <div className="container text-center bg-danger">
+                    <div className="row">
 
-                    <div className="col-md-4 p-4 m-3 " >
+                    <div className="col-md-5 p-5 m-3" >
                         <h2 className="default-text">Gombos Benedek Zsombor</h2>
 
                         <div className=" d-flex justify-content-center">
                             <div className='h-25 mb-4'>
                                 <a target='_blank' rel='noopener noreferrer' href="https://www.youtube.com/@kocsonyaleves"><img className='w-75 black-icon' src={youtubeIcon} alt="" /></a>
-=======
-            <h3 className="main-brand mt-5 m-auto">Made by:</h3>
-            <div className="container text-center">
-                    <div className="mt-5 horizontal">
-                        <div className="g-col-6 p-3 about-item" >
-                            <h2 className="default-text m-auto">Gombos Benedek Zsombor</h2>
-                            <div className=" d-flex justify-content-center">
-                                <div className='h-25 mb-4'>
-                                    <a target='_blank' rel='noopener noreferrer' href="https://www.youtube.com/@kocsonyaleves"><img className='w-75 black-icon' src={youtubeIcon} alt="" /></a>
-                                </div>
-                                <div className='h-25 mb-4'>
-                                    <a target='_blank' rel='noopener noreferrer' href="https://github.com/kiChi-420"><img className='w-75 black-icon' src={githubIcon} alt="" /></a>
-                                </div>
                             </div>
-                        </div>
-
-                        <div className="g-col-6 p-3">
-                            <h2 className="m-auto">Vasilescu András László</h2>
-                            <div className="d-flex justify-content-center  ">
-                                <div className='h-25 mb-4'>
-                                    <a target='_blank' rel='noopener noreferrer' href="https://www.youtube.com/@FreestyleMaci"><img className='w-75 black-icon' src={youtubeIcon} alt="" /></a>
-                                </div>
-                                <div className='h-25 mb-4'>
-                                    <a target='_blank' rel='noopener noreferrer' href="https://github.com/andrasvas"><img className='w-75 black-icon' src={githubIcon} alt="" /></a>
-                                </div>
-                            </div>
-                            
-                        </div>
-                        <div className="g-col-6 p-3">
-                            <h2 className="m-auto">Oláh Gergő</h2>
-
-                            <div className="d-flex justify-content-center">
-                                <div className='h-25 mb-4'>
-                                    <a target='_blank' rel='noopener noreferrer' href="https://www.instagram.com/atreyoos?igsh=ang4NWs0Y2hiZzVl"><img className='w-75 black-icon' src={instaIcon} alt="" /></a>
-                                </div>
-                                <div className='h-25 mb-4'>
-                                    <a target='_blank' rel='noopener noreferrer' href="https://www.youtube.com/@FreestyleMaci"><img className='w-75 black-icon' src={youtubeIcon} alt="" /></a>
-                                </div>
-                                <div className='h-25 mb-4'>
-                                    <a target='_blank' rel='noopener noreferrer' href="https://github.com/OlaGergo"><img className='w-75 black-icon' src={githubIcon} alt="" /></a>
-                                </div>
->>>>>>> Stashed changes
-                            </div>
+                            <div className='h-25 mb-4'>
+                                <a target='_blank' rel='noopener noreferrer' href="https://github.com/kiChi-420"><img className='w-75 black-icon' src={githubIcon} alt="" /></a>
                             </div>
                         </div>
                     </div>
-<<<<<<< Updated upstream
-                    <div className="col-md-3 p-4 m-3">
+                    <div className="col-md-5 p-5 m-3">
                         <h2>Vasilescu András László</h2>
 
                         <div className="d-flex justify-content-center  ">
@@ -132,7 +70,7 @@ function About(){
                         </div>
                         
                     </div>
-                    <div className="col-md-4 p-5 m-3 ">
+                    <div className="col-md-5 p-5 m-3">
                         <h2>Oláh Gergő </h2>
 
                         <div className="d-flex justify-content-center pa3  ">
@@ -148,9 +86,8 @@ function About(){
                         </div>
                     </div>
                 </div>
-=======
->>>>>>> Stashed changes
             </div>
+        </div>
     )
 
 }
